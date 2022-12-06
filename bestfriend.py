7e750d4e5b3cb7ccddf002a6da2451234155dd62
Elijah Beerman <ebeerman19@gmail.com>
2022-12-06 14:21:12 -0500

    Hello

--- bestfriend.py
+++ bestfriend.py
@@ -1,4 +1,14 @@
 first_name = input("What is your name? ")
 hellomario="Hello, Mario"
 print(f"{hellomario}")
-print(f"Hello, {first_name} {hellomario}") 
+print(f"Hello, {first_name}") 
+Second_name= input("How was you day? ")
+print(f"{Second_name}")
+Third_name = input("bomb north korea? ")
+print(f"{Third_name}")
+if Third_name == "Yes":
+    print("YES")
+else:
+    print("YOU SUCK")
+Forth_name = input("Russia now?")
+print(f"{Forth_name}")
